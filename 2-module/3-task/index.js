73d655238d5097e8ef07d13f4522979163bfdeeb
NinaNez <ninanezvanova94@gmail.com>
2025-05-25 14:29:15 +0300

    калькулятор

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,17 @@
-let calculator = {
-  // ваш код
-};
+  let calculator = {
 
-// НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
+    read(){
+      this.a = +prompt('a?', 0);
+      this.b = +prompt('b?', 0);
+    },
+
+    sum(){
+      return this.a + this.b;
+    },
+
+    mul(){
+      return this.a * this.b;
+    }
+  }
+
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
