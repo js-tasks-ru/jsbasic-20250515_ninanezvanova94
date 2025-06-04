00a7f888c17972d50169b918804e3f75e0a3c586
NinaNez <ninanezvanova94@gmail.com>
2025-06-04 19:29:24 +0300

    Преобразование массива в html список

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,7 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('ul');
+
+  ul.innerHTML = friends.map(item=>`<li>${item.firstName+' '+item.lastName}</li>`).join('');
+  
+  return ul;
 }
